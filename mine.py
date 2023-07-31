1317ba21661a90c177b0e23f3b4474df4be9fae2
ZeRo <109825197+ZeRo-RX@users.noreply.github.com>
2023-07-31 23:46:07 +0430

    ZeRo2

--- mine.py
+++ mine.py
@@ -2,7 +2,7 @@ import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 
 # خواندن داده‌های CSV
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data3.csv')
 
 # تبدیل ستون date به فرمت datetime
 data['date'] = pd.to_datetime(data['date'])
@@ -11,11 +11,11 @@ data['date'] = pd.to_datetime(data['date'])
 data = data.set_index('date')
 
 # ایجاد مدل ARIMA برای پیش‌بینی دما
-model_temp = ARIMA(data['temp'], order=(1, 1, 1))
+model_temp = ARIMA(data['tmin'], order=(1, 0, 0))
 model_temp_fit = model_temp.fit()
 
 # ایجاد مدل ARIMA برای پیش‌بینی رطوبت
-model_humid = ARIMA(data['humidity'], order=(1, 1, 1))
+model_humid = ARIMA(data['tmax'], order=(1, 0, 0))
 model_humid_fit = model_humid.fit()
 
 # پیش‌بینی دما و رطوبت برای 10 روز آینده
@@ -23,5 +23,5 @@ temp_forecast = model_temp_fit.forecast(steps=3)
 humid_forecast = model_humid_fit.forecast(steps=3)
 
 # چاپ پیش‌بینی دما و رطوبت برای 10 روز آینده
-print('Temperature Forecast:', temp_forecast)
-print('Humidity Forecast:', humid_forecast)
+print('tmin:' , temp_forecast)
+print('tmax:', humid_forecast)
